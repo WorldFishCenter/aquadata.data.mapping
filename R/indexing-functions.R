0461a50b590931828c5587c54a1d6e70b903c7a3
langbart <lorenzo.longobardi@gmail.com>
2023-01-11 19:10:47 +0100

    tolower word before filtering

--- R/indexing-functions.R
+++ R/indexing-functions.R
@@ -2,14 +2,15 @@
 #'
 #' Filter metadata by a specific word in dataset title and/or dataset keyword list.
 #'
-#' @param data Metadata dataframe.
 #' @param word Character string to be be filtered.
 #'
 #' @return A dataframe
 #' @export
 #' @importFrom rlang .data
 #'
-filterby_word <- function(data = NULL, word = NULL){
-  data |>
-    dplyr::filter(grepl(word, .data$title) | .data$keyword_value == word)
+filterby_word <- function(word = NULL) {
+  tol_word <- tolower(word)
+
+  aquadata.data.mapping::dataverse_metadata %>%
+    dplyr::filter(grepl(tol_word, .data$title) | .data$keyword_value == tol_word)
 }
