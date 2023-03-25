c5521aedea4e8290caa0cec09acdec45a7cb11f6
langbart <lorenzo.longobardi@gmail.com>
2023-03-25 02:34:26 +0100

    Store chatgpt refine prompts into conf file

--- R/chatgpt-wraps.R
+++ R/chatgpt-wraps.R
@@ -1,20 +1,33 @@
 #' Summarise document using LangChain and ChatGPT
 #'
-#' This function is a R wrapper of `chatgpt_chaindoc.py`, a python function aimed
-#' to summarise large text documents using LangChain and ChatGPT.
+#' This function is a R wrapper of `chatgpt_chaindoc.py`, a python script function
+#' aimed to elaborate large text documents using LangChain and ChatGPT.
+#' `chatgpt_chaindoc.py` uses the refine chain method to elaborate the text
+#' (see \url{https://langchain.readthedocs.io/en/latest/modules/indexes/chain_examples/summarize.html#the-refine-chain} for details).
 #'
 #' @param document_path The path of the txt document to summarise.
 #' @param openaikey The OpenAI key token.
 #' @param engine ChatGPT model (see \url{https://platform.openai.com/docs/models}).
-#' @param temperature Level of randomness or "creativity" in the generated text (see \url{https://platform.openai.com/docs/api-reference/completions/create})
+#' @param temperature Level of randomness or "creativity" in the generated text (see \url{https://platform.openai.com/docs/api-reference/completions/create}).
+#' @param refine_text A text useful to refine the original prompt (see \url{https://langchain.readthedocs.io/en/latest/modules/indexes/chain_examples/summarize.html#the-refine-chain} for details).
 #'
 #' @return A summarised text.
 #' @export
-#'
-
-summarise_chatgpt_wrapper <- function(document_path, openaikey, engine, temperature) {
+#' @examples
+#' \dontrun{
+#' output <-
+#' summarise_chatgpt_wrapper(
+#' document_path = "inst/docs_dataverse/5636634.txt",
+#' openaikey = OPENAI_KEY,
+#' engine = "gpt-3.5-turbo",
+#' temperature = 0.7,
+#' refine_text = pars$openai$refine_prompts$p1
+#' )
+#' cat(output$output_text)
+#' }
+summarise_chatgpt_wrapper <- function(document_path, openaikey, engine, temperature, refine_text) {
   chatgpt_chain_py <- reticulate::import("chatgpt_chaindoc")
   py_function <- chatgpt_chain_py$summarize_document
-  result <- py_function(document_path, openaikey, engine, temperature)
+  result <- py_function(document_path, openaikey, engine, temperature, refine_text)
   return(result)
 }
