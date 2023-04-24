8efe712d76f44f90f3c1750c8a9d41bafb288c5e
langbart <lorenzo.longobardi@gmail.com>
2023-04-24 18:47:52 +0200

    try fixing data-raw path

--- inst/python/get_dataverse_metadata.py
+++ inst/python/get_dataverse_metadata.py
@@ -398,7 +398,9 @@ def get_dataverse_metadata(organization):
     # Create CSV of all metadata by joining CSV files in the csv_files folder
 
     # Create csv file in the directory that the user selected
-    filename = 'python/%s_dataset_metadata_%s.csv' % (alias, current_time)
+    #filename = 'data-raw/%s_dataset_metadata_%s.csv' % (alias, current_time)
+    current_dir = os.path.abspath('.')
+    filename = os.path.join(current_dir, 'data-raw', '%s_dataset_metadata_%s.csv' % (alias, current_time))
 
     # Save directory paths to each csv file as a list and save in 'all_tables'
     # variable
