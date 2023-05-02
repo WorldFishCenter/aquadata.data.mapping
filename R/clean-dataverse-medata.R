830720f088d526ac42c06740f7b4888a2366a158
langbart <lorenzo.longobardi@gmail.com>
2023-05-02 09:08:13 +0200

    clean raw data using file paths without specifying folder

--- R/clean-dataverse-medata.R
+++ R/clean-dataverse-medata.R
@@ -2,15 +2,14 @@
 #'
 #' Read and clean Dataverse raw metadata files.
 #'
-#' @param folder_path Path of folder where raw metadata are located.
 #' @param file_path Path of raw metadata file.
 #'
 #' @return A dataframe.
 #' @export
 #' @importFrom rlang .data
 #'
-clean_metadata_files <- function(folder_path = NULL, file_path = NULL) {
-  readr::read_csv(paste0(folder_path, "/", file_path), show_col_types = FALSE) %>%
+clean_metadata_files <- function(file_path = NULL) {
+  readr::read_csv(file_path, show_col_types = FALSE) %>%
     dplyr::rename(
       dataset_doi = .data$persistentUrl,
       publication_date = .data$publicationdate
@@ -43,13 +42,12 @@ process_raw_metadata <- function(log_threshold = logger::DEBUG) {
   pars <- read_config()
 
   path <- system.file("data-raw", package = "aquadata.data.mapping")
-  csv_files <- list.files(path)
-  metadata_files <- csv_files[c(which(grepl("dataset_metadata", csv_files)))]
+  metadata_files <- list.files(path, full.names = TRUE)
   org_names <- stringr::word(metadata_files, 1, sep = "\\_")
 
   logger::log_info("Cleaning metadata raw data")
   dataverse_metadata <-
-    purrr::map(metadata_files, clean_metadata_files, folder_path = path) %>%
+    purrr::map(metadata_files, clean_metadata_files) %>%
     rlang::set_names(org_names) %>%
     dplyr::bind_rows(.id = "organization") %>%
     janitor::remove_empty(c("rows", "cols")) %>%
